4eacd656babc517aa16528670ab93687e7e72497
Pietro907 <pietrop.puri@gmail.com>
2023-10-12 15:32:39 +0200

    add initial page

---
+++ index.php
@@ -0,0 +1,44 @@
+<?php
+
+/*
+
+Descrizione
+Dobbiamo creare una web-app che permetta di leggere una lista di dischi presente nel nostro server.
+
+Stack
+Html, CSS, VueJS (importato tramite CDN), axios, PHP
+
+Consigli
+Nello svolgere l’esercizio seguite un approccio graduale.
+
+Prima assicuratevi che la vostra pagina index.php (il vostro front-end) riesca a comunicare correttamente con il vostro script PHP (le vostre “API”).
+
+Solo a questo punto sarà utile passare alla lettura della lista da un file JSON.
+
+Bonus (da fare entro domani prima della correzione)
+Al click su un disco, recuperare e mostrare i dati del disco selezionato.
+
+ */
+
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHP Dischi JSON</title>
+    <link rel="stylesheet" href="./style.css">
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/axios/1.5.1/axios.min.js" integrity="sha512-emSwuKiMyYedRwflbZB2ghzX8Cw8fmNVgZ6yQNNXXagFzFOaQmbvQ1vmDkddHjm5AITcBIZfC7k4ShQSjgPAmQ==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>                                            
+</head>
+
+<body>
+
+
+
+
+    <script src="https://unpkg.com/vue@3/dist/vue.global.js"></script>
+</body>
+
+</html>
