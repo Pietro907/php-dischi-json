72565cf983d1de6cc6e65c71adbc02ec490ea171
Pietro907 <pietrop.puri@gmail.com>
2023-10-12 18:07:54 +0200

    add try bonus but not success

--- index.php
+++ index.php
@@ -53,6 +53,24 @@ Al click su un disco, recuperare e mostrare i dati del disco selezionato.
 
                     </div>
 
+                    <div v-for="(task, index) in todos" :key="task + index" class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+                    <div class="modal-dialog m_auto">
+                        <div class="modal-content width">
+                            <div class="modal-header">
+                                <h1 class="modal-title fs-5" id="exampleModalLabel">Modal title</h1>
+                                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                            </div>
+                            <div class="modal-body">
+                            {{task.title}}
+                            </div>
+                            <div class="modal-footer">
+                                <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+                                <button type="button" class="btn btn-primary">Save changes</button>
+                            </div>
+                        </div>
+                    </div>
+
+                </div>
                 </div>
 
                 <!-- Button trigger modal -->
@@ -61,23 +79,7 @@ Al click su un disco, recuperare e mostrare i dati del disco selezionato.
                 </button>
 
                 <!-- Modal -->
-                <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
-                    <div class="modal-dialog">
-                        <div class="modal-content width">
-                            <div class="modal-header">
-                                <h1 class="modal-title fs-5" id="exampleModalLabel">Modal title</h1>
-                                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-                            </div>
-                            <div class="modal-body">
-                                ...
-                            </div>
-                            <div class="modal-footer">
-                                <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
-                                <button type="button" class="btn btn-primary">Save changes</button>
-                            </div>
-                        </div>
-                    </div>
-                </div>
+                
 
             </div>
         </div>
