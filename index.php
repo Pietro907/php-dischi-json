39bb6b68df344236a61517b4be0624cccaffd481
Pietro907 <pietrop.puri@gmail.com>
2023-10-12 17:47:11 +0200

    add on click appare a div with image

--- index.php
+++ index.php
@@ -29,41 +29,66 @@ Al click su un disco, recuperare e mostrare i dati del disco selezionato.
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>PHP Dischi JSON</title>
-    <link rel="stylesheet" href="./style.css">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
-                                                
+    <link rel="stylesheet" href="./style.css">
+
 </head>
 
 
 <body>
 
-<div id="app">
+    <div id="app">
 
-    <div class="container">
-        <div class="row">
+        <div class="container">
+            <div class="row">
 
-            <div v-for="(task, index) in todos" :key="task + index" class="col-2">
+                <div v-for="(task, index) in todos" :key="task + index" class="col-2">
 
-                <div class="card d-flex align-items-center">
+                    <div class="card d-flex align-items-center">
 
-                    <img class="poster" :src="task.poster" alt="">
-                    <h6>{{task.title}}</h6>
-                    <p>{{task.author}}</p>
-                    <h6>{{task.year}}</h6>
+                        <img class="poster" :src="task.poster" alt="" data-bs-toggle="modal" data-bs-target="#exampleModal">
+                        <h6>{{task.title}}</h6>
+                        <p>{{task.author}}</p>
+                        <h6>{{task.year}}</h6>
+
+                    </div>
 
                 </div>
 
+                <!-- Button trigger modal -->
+                <button type="button" class="btn btn-primary">
+                    Launch demo modal
+                </button>
+
+                <!-- Modal -->
+                <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+                    <div class="modal-dialog">
+                        <div class="modal-content width">
+                            <div class="modal-header">
+                                <h1 class="modal-title fs-5" id="exampleModalLabel">Modal title</h1>
+                                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                            </div>
+                            <div class="modal-body">
+                                ...
+                            </div>
+                            <div class="modal-footer">
+                                <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+                                <button type="button" class="btn btn-primary">Save changes</button>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
             </div>
-           
         </div>
+
     </div>
 
-</div>
 
-
-<script src="https://unpkg.com/vue@3/dist/vue.global.js"></script>
-<script src="https://cdnjs.cloudflare.com/ajax/libs/axios/1.5.1/axios.min.js" integrity="sha512-emSwuKiMyYedRwflbZB2ghzX8Cw8fmNVgZ6yQNNXXagFzFOaQmbvQ1vmDkddHjm5AITcBIZfC7k4ShQSjgPAmQ==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
-<script src="./main.js"></script>
+    <script src="https://unpkg.com/vue@3/dist/vue.global.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/axios/1.5.1/axios.min.js" integrity="sha512-emSwuKiMyYedRwflbZB2ghzX8Cw8fmNVgZ6yQNNXXagFzFOaQmbvQ1vmDkddHjm5AITcBIZfC7k4ShQSjgPAmQ==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
+    <script src="./main.js"></script>
 </body>
 
 </html>
